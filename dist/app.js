d2f563f0e9fb20a8a8fcea6babe8d80661aa0347
Jack Birch <jack.birch99@outlook.com>
2023-03-15 18:54:16 +0000

    [ADD] Allow the server to connect to a remote mongodb database.

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,44 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const connect_1 = __importDefault(require("./db/connect"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
+// Port number on which server will run
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
+/**
+ * Check that the mongoURI is set, then connect to the database
+ * Once connected to the db, load the server
+ */
+const start = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const mongoURI = process.env.MONGO_URI;
+        if (typeof mongoURI === "undefined") {
+            throw new Error("Mongo URI is undefined");
+        }
+        yield (0, connect_1.default)(mongoURI);
+        app.listen(port, () => { console.log(`Server is listening on port ${port}...`); });
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
+if (require.main === module) {
+    start();
+}
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import connectDB from './db/connect';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,7 +9,28 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
+// Port number on which server will run
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
-});
+
+/**
+ * Check that the mongoURI is set, then connect to the database
+ * Once connected to the db, load the server
+ */
+const start = async() => {
+    try{
+        const mongoURI = process.env.MONGO_URI;
+        if (typeof mongoURI === "undefined"){
+            throw new Error("Mongo URI is undefined");
+        }
+        await connectDB(mongoURI);
+        app.listen(port, () => {console.log(`Server is listening on port ${port}...`);});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+if (require.main === module){
+    start();
+}
+
+
