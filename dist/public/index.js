c77092386320fb1e53f12c4476a06f09aa3a72bb
Jack Birch <jack.birch99@outlook.com>
2023-04-22 18:14:41 +0100

    [IMP] Update items in the list of foods.

--- dist/public/index.js
+++ dist/public/index.js
@@ -1,3 +1,5 @@
+// Javascript file that uses axios to send requests to the backend server to CRUD items in the food list
+
 const submitBtn = document.querySelector(".submit-btn");
 const foodName = document.querySelector("#name");
 const quantity = document.querySelector("#quantity");
@@ -5,26 +7,55 @@ const unitOfMeasure = document.querySelector("#unit-of-measure");
 const foodList = document.querySelector(".list-food");
 const deleteAllBtn = document.querySelector("#delete-all-btn");
 
-// Creating Food 
+// Global Edit Variable
+let editFlag = false
+let id = 0;
+
+////// CREATING AND EDITING FOOD
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const food = {name:foodName.value,quantity:quantity.value,unitOfMeasure:unitOfMeasure.value};
-    createFood(food);
+    if (editFlag){
+        editFood(food);
+    } else {
+        createFood(food);
+    }
 })
 
-// We add functionality to send a request to the server and create our food item
 const createFood = async(food) => {
     try{
         const newFood = await axios.post('/api/v1/foods/', food)
-        // Add all foods to the list
         createFoodElement(newFood.data); 
     } catch (e) {
         console.log(e);
     }
 }
 
-// Listing Foods
+/**
+ * Update the food object in the database and update the food item in the list of foods
+ * @param {*} foodObj - The food data type
+ */
+const editFood = async(foodObj) => {
+    try {
+        const updatedFood = await axios.patch(`/api/v1/foods/${id}`, foodObj)
+        let foods = Array.from(foodList.children)
+        foods = foods.filter((item) => {
+            return item.dataset.id === updatedFood.data._id;
+        })
+        console.log(foods);
+        if (foods || food.length === 1) {
+            foodList.removeChild(foods[0]);
+            createFoodElement(updatedFood.data);
+        };
+        _reset_edit_variables();
+        
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+////// LISTING FOOD
 
 window.addEventListener('DOMContentLoaded', () => {
     getAllFoods()
@@ -40,7 +71,7 @@ const getAllFoods = async() => {
 }
 
 /**
- * Create a food element in the food list, with a delete button
+ * Create a food element in the food list, with a delete button and an edit button.
  * @param {*} food - A mongodb record 
  */
 const createFoodElement = (food) => {
@@ -55,24 +86,63 @@ const createFoodElement = (food) => {
         deleteFood(deleteBtn.parentElement);
     })
 
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.classList.add("edit-single-btn");
+    editBtn.addEventListener('click', () => {
+        _set_edit_variables(food);
+    })
+
+    listEl.appendChild(editBtn);
     listEl.appendChild(deleteBtn);
     foodList.appendChild(listEl);
 }
 
-// Deleting Foods 
+////// DELETING FOOD 
 
+/** 
+ * Get all child elements and convert to an array so they can 
+ * be iterated over foodList.children returns a HTMLCollection 
+ */
 deleteAllBtn.addEventListener('click', () => {
-    // Get all child elements and convert to an array so they can  be iterated over
-    // foodList.children returns a HTMLCollection
     const foods = Array.from(foodList.children)
     foods.forEach(deleteFood)
 })
 
-const deleteFood = async(food) => {
+const deleteFood = async(foodElement) => {
     try{
-        await axios.delete(`/api/v1/foods/${food.dataset.id}`);
-        foodList.removeChild(food);
+        await axios.delete(`/api/v1/foods/${foodElement.dataset.id}`);
+        foodList.removeChild(foodElement);
     } catch (e) {
         console.log(e)
     }
+}
+
+////// UPDATING GLOBAL VARIABLES 
+
+/**
+ * Reset the webpage back for creating, the global variables should only be changed 
+ * through this function and _set_edit_variables.
+ */
+const _reset_edit_variables = () => {
+    editFlag = false;
+    submitBtn.innerHTML = "Submit";
+    id = 0;
+    foodName.value = "";
+    quantity.value = "";
+    unitOfMeasure.value = "";
+}
+
+/**
+ * Set the webpage up for editing, the global variables should only be changed through
+ * this function and _reset_edit_variables.
+ * @param {} food - Mongo DB record
+ */
+const _set_edit_variables = (food) => {
+    editFlag = true;
+    submitBtn.innerHTML = "Confirm Edit";
+    id = food._id;
+    foodName.value = food.name;
+    quantity.value = food.quantity;
+    unitOfMeasure.value = food.unitOfMeasure;
 }
