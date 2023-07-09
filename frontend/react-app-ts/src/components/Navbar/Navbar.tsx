986bd575e604d7da850f3cf788b6525e05bc0f34
Jack Birch <jack.birch99@outlook.com>
2023-07-09 15:47:07 +0100

    [FIX] react-app-ts: Add further styling improvements.

--- frontend/react-app-ts/src/components/Navbar/Navbar.tsx
+++ frontend/react-app-ts/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
 
 import { useGlobalContext } from "../../Context";
 
@@ -7,6 +9,10 @@ import NavbarStyles from "./NavbarStyles.module.css";
 
 const Navbar = () => {
   const { isLoggedIn, setAndClearIsLoggedIn, setAndClearUsername, username } = useGlobalContext();
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef<HTMLDivElement>(null);
+  const linksRef = useRef<HTMLDivElement>(null);
+  
 
   const navigate = useNavigate();
 
@@ -25,20 +31,38 @@ const Navbar = () => {
     navigate("/");
   };
 
+  useEffect(() => {
+    if(!linksRef.current) return;
+    if(!linksContainerRef.current) return;
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    console.log(linksHeight);
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav className={NavbarStyles.nav}>
+      <div className={NavbarStyles.main_headings}>
       {isLoggedIn ? <h1>{username}'s Food App</h1> : <h1>Food App</h1>}
-      <div className={NavbarStyles.links}>
-      {/* Conditionally render login / log out button */}
-      <Link to="/" className={NavbarStyles.link}>Home</Link>
-      {isLoggedIn ? (
-          <>
-            <Link to="/calendar" className={NavbarStyles.link}>Calendar</Link>
-            <button onClick={handleLogout} className={NavbarStyles.link}>Logout</button>
-          </>
-      ) : (
-          <button onClick={() => navigate("/login")} className={NavbarStyles.link}>Log In</button>
-      )}
+
+      <button onClick={()=>setShowLinks(!showLinks)} className={NavbarStyles.button}><FaBars/></button>
+      </div>
+      <div ref={linksContainerRef} className={NavbarStyles.links_wrapper}>
+        <div ref={linksRef} className={NavbarStyles.links}>
+          {/* Conditionally render login / log out button */}
+          <Link to="/" className={NavbarStyles.link}>Home</Link>
+          {isLoggedIn ? (
+              <>
+                <Link to="/calendar" className={NavbarStyles.link}>Calendar</Link>
+                <button onClick={handleLogout} className={NavbarStyles.link}>Logout</button>
+              </>
+          ) : (
+              <button onClick={() => navigate("/login")} className={NavbarStyles.link}>Log In</button>
+          )}
+        </div>
       </div>
     </nav>
   );
